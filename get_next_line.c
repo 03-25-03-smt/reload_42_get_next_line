7e945f6f68be072df7bd7c92d19ba4212504d66a
03-25-03-smt <vlad40303@gmail.com>
2026-09-21 21:45:41 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -8,3 +8,13 @@ char	*get_next_line(int fd)
 	(void)stash;
 	return (NULL);
 }
+
+static ssize_t	read_buffer(int fd, char *buffer)
+{
+	ssize_t	bytes_read;
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read > 0)
+		buffer[bytes_read] = '\0';
+	return (bytes_read);
+}
