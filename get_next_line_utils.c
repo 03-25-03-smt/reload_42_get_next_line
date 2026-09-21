43222316edc66a29d29de928941c730a4d3e0326
03-25-03-smt <vlad40303@gmail.com>
2026-09-21 21:42:13 +0200

    Create get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,28 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&str[i]);
+	return (NULL);
+}
+
