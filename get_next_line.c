f62d818d27cbd6e495553925f7f3da3138b6d1ad
03-25-03-smt <vlad40303@gmail.com>
2026-09-21 21:46:07 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -18,3 +18,26 @@ static ssize_t	read_buffer(int fd, char *buffer)
 		buffer[bytes_read] = '\0';
 	return (bytes_read);
 }
+
+static char	*read_to_stash(int fd, char *stash)
+{
+	char		*buffer;
+	ssize_t		bytes_read;
+	char		*tmp;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_read = read_buffer(fd, buffer);
+	while (bytes_read > 0)
+	{
+		tmp = ft_strjoin(stash, buffer);
+		free(stash);
+		stash = tmp;
+		if (ft_strchr(buffer, '\n'))
+			break;
+		bytes_read = read_buffer(fd, buffer);
+	}
+	free(buffer);
+	return (stash);
+}
