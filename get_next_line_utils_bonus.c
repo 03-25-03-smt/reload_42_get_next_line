3fc61fcb0132cd8854558071dc0a50e4d33a8b6a
03-25-03-smt <vlad40303@gmail.com>
2026-09-23 10:29:24 +0200

    Update get_next_line_utils_bonus.c

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,1 +1,90 @@
+#include "get_next_line.h"
 
+char  *ft_strchr(const char *s, int c)
+{
+  size_t  i;
+
+  i = 0;
+  while (s[i] != '\'0')
+  {
+    if (s[i] == (char)c)
+      return ((char)&s[i]);
+    i++;
+  }
+  if ((char)c == '\0')
+    return ((char)&s[i]);
+  return (0);
+}
+
+size_t ft_strlen(const char *s)
+{
+  size_t  i;
+
+  i = 0;
+  while (s[i] != '\0')
+    i++;
+  return (i);
+}
+
+char *ft_strdup(char *str)
+{
+  size_t  len;
+  size_t  i;
+  char  *new_str;
+
+  len = ft_strlen(str);
+  new_str = malloc(len + 1);
+  if (!new_str)
+    return (NULL);
+  i = 0;
+  while (str[i] != '\0')
+  {
+    new_str[i] = str[i];
+    i++;
+  }
+  new_str[i] = '\0';
+  return (new_str);
+}
+
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+  size_t  i;
+  size_t  dst_len;
+
+  dst_len = 0;
+  while (dst_len < dstsize && dst[dst_len])
+    dst_len++;
+  if (dst_len == dstsize)
+    return (dstsize + ft_strlen(src));
+  i = 0;
+  while (dst_len + i < dstsize - 1 && src[i] != '\0')
+  {
+    dst[dst_len + i] = src[i];
+    i++;
+  }
+  dst[dst_len + i] = '\0';
+  return (dst_len + ft_strlen(src));
+}
+  
+char *ft_strjoin(char const *s1, char const *s2)
+{
+  size_t  len_s1;
+  size_t  len_s2;
+  char	*new;
+
+  len_s1 = 0;
+  len_s2 = 0;
+  if (s1 != NULL)
+		len_s1 = ft_strlen(s1);
+  if (s2 != NULL)
+		len_s2 = ft_strlen(s2);
+  new = malloc(len_s1 + len_s2 + 1);
+	if (!new)
+		return (NULL);
+  new[0] = '\0';
+	if (s1)
+		ft_strlcat(new, s1, len_s1 + len_s2 + 1);
+	if (s2)
+		ft_strlcat(new, s2, len_s1 + len_s2 + 1);
+	return (new);
+}
