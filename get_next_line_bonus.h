841c6349175667fa73fd695492abd6b9744458b7
03-25-03-smt <vlad40303@gmail.com>
2026-09-23 10:28:12 +0200

    Create get_next_line_bonus.h

---
+++ get_next_line_bonus.h
@@ -0,0 +1,19 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <sys/select.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+#endif
